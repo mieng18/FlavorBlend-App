af9a2113a3995801c4beda3ca6c135861cdaeaf2
mieng18 <mieng18@hotmail.com>
2024-05-30 15:48:09 -0700

    Initial commit

---
+++ FlavorBlend-App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlavorBlend-App
+//
+//  Created by mai nguyen on 5/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlavorBlend-App/FlavorBlend_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorBlend_AppApp.swift
+//  FlavorBlend-App
+//
+//  Created by mai nguyen on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct FlavorBlend_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
